1c429698bbc2e1c3be9fbd009c39fd454ef128d4
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-04-20 19:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+	let chars = "ABCDEFGIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+	let id = "";
+	for(let i=0;i<=l;i++) {
+		let rndmIdx = parseInt(Math.random()*chars.length);
+		let rndmVal = chars[rndmIdx];
+		id = id + rndmVal;
+	}
+	return id;
 }
 
 // Do not change the code below.
